945be64e288322078a17631feb066cddfe9ec6aa
EmeraldSeas <115032064+EmeraldSeas@users.noreply.github.com>
2023-07-18 07:28:41 -0400

    f-string

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ totalBill = float(input("What was the total bill? $"))  # accept total bill as f
 paySplit = int(input("How many people to split the bill?\n"))  # accept number of people paying as int
 tipPercentage = float(int(input("What percentage do you want to tip? 12, 15, or 20\n")) / 100)
 tip = round((totalBill / paySplit) * tipPercentage, 2)  # calculate 20% tip based on number of people tipping
-print("Each person should tip: $" + str(tip))  # display tip
+print(f"Each person should tip ${str(tip)}")  # display tip
