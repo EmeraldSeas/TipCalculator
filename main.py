48738e19d9c7223f6fbb761823a4da2f623fc12e
EmeraldSeas <115032064+EmeraldSeas@users.noreply.github.com>
2023-07-18 07:13:04 -0400

    variable tip percentage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Welcome to the tip calculator.")  # print welcome message
 totalBill = float(input("What was the total bill? $"))  # accept total bill as float
-paySplit = int(input("How many people to split the bill? "))  # accept number of people paying as int
-tip = round((totalBill / paySplit) * .20, 2)  # calculate 20% tip based on number of people tipping
-print("Each person should tip: $" + str(tip)) # display tip
+paySplit = int(input("How many people to split the bill?\n"))  # accept number of people paying as int
+tipPercentage = float(int(input("What percentage do you want to tip? 12, 15, or 20\n")) / 100)
+tip = round((totalBill / paySplit) * tipPercentage, 2)  # calculate 20% tip based on number of people tipping
+print("Each person should tip: $" + str(tip))  # display tip
